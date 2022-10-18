03ec06437e6175955280708ed7f69169902dcc72
Justin Taylor <ju.tay.lor2019@gmail.com>
2022-10-17 20:55:58 -0400

    changed initials to be its own div

--- portfolio/src/screens/section1.js
+++ portfolio/src/screens/section1.js
@@ -4,8 +4,11 @@ export function Screen1() {
 
     return (
         <div className="mainContentContainer">
+            <div className="initials">
+                <h1 className="j">J</h1>
+                <h1>T</h1>
+            </div>
             <div className="leftContainer">
-                    <h1>J</h1>
                 <div className="verticalText">
                     <h3>U</h3>
                     <h3>S</h3>
@@ -15,7 +18,6 @@ export function Screen1() {
                 </div>
             </div>
             <div className="rightContainer">
-                <h1>T</h1>
                 <div className="horizontalText">
                     <h3>A</h3>
                     <h3>Y</h3>
@@ -24,6 +26,7 @@ export function Screen1() {
                     <h3>R</h3>
                 </div>
             </div>
+            
             {/* Tropical trees font */}
             {/* <a href="/"><img src="https://see.fontimg.com/api/renderfont4/d9VDV/eyJyIjoiZnMiLCJoIjoxMDcsInciOjEwMDAsImZzIjoxMDcsImZnYyI6IiMwMDAwMDAiLCJiZ2MiOiIjRkZGRkZGIiwidCI6MX0/SlQ/palmsprings-personal-use-regular.png" alt="Logo fonts"/></a> */}
         </div>
