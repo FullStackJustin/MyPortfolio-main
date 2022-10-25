cb6456678ada1131d599bb61769c3d4515c2763b
Justin Taylor <ju.tay.lor2019@gmail.com>
2022-10-25 19:53:53 -0400

    added aos to project for fading styles

--- portfolio/src/screens/section3.js
+++ portfolio/src/screens/section3.js
@@ -12,9 +12,11 @@ export const Screen3 = () => {
                     <form>
                         <header><h3>Contact Me</h3></header>
                         <main>
-                            <label types="text" for="emailInput">Enter Your Email</label>
-                            <input id="emailInput" placeholder="Send me an email"></input>
-                            <label types="text" for="messageInput">Leave me a Message</label>
+                            <label>Enter Your Email</label>
+                            <input id="emailInput" placeholder="Type your email"></input>
+                            <br></br>
+                            <br></br>
+                            <label>Leave me a Message</label>
                             <input id="messageInput" placeholder="Type a comment, Question, or Message"></input>
                         </main>
                     </form>
