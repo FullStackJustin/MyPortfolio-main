c33e28fec5595eea056112b902dc1248ad86f498
Justin Taylor <ju.tay.lor2019@gmail.com>
2022-10-27 18:01:02 -0400

    added emailjs send email function to contact page

--- portfolio/src/screens/section3.js
+++ portfolio/src/screens/section3.js
@@ -1,24 +1,42 @@
 import "../styles/section3.css"
+import React from "react";
 import { Footer } from "../pages/footer"
+import emailjs from '@emailjs/browser';
 
 export const Screen3 = () => {
 
+    const form = React.useRef();
+    const serviceID = process.env.REACT_APP_SERVICE_ID;
+    const templateID = process.env.REACT_APP_TEMPLATE_ID;
+    const publicKey = process.env.REACT_APP_accessToken;
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs.sendForm(serviceID, templateID, form.current, publicKey)
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error);
+          });
+      };
     
     
     
     return(
         <div id="contactPageContainer">
             <div className="contactCard">
-                <form id="contactForm">
+                <form id="contactForm" ref={form} onSubmit={sendEmail}>
                     <header><h3>Contact Me</h3></header>
                     <main>
                         <label>Enter Your Email</label>
-                        <input id="emailInput" placeholder="Type your email"></input>
+                        <input id="emailInput" type="email" placeholder="Type your email"></input>
                         <br></br>
                         <br></br>
                         <label>Leave me a Message</label>
                         <input id="messageInput" placeholder="Type a comment, Question, or Message"></input>
                     </main>
+                    <button type="submit">Send Message</button>
                 </form>
             </div>
             <Footer/>
