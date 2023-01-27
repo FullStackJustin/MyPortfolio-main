8dda9022759e6d5aeaa0dc05ecb16ebe8f7a24f3
Justin Taylor <ju.tay.lor2019@gmail.com>
2023-01-26 21:47:30 -0800

    added endpoints to change user view for better readability on section 2

--- portfolio/src/screens/section2.js
+++ portfolio/src/screens/section2.js
@@ -44,12 +44,6 @@ export const Screen2 = () => {
                     <form className="projectsForm">
                         <main>
                             <div className="dropdown">
-                                <a className="dropbtn" href="https://github.com/FullStackJustin/Cool-Calculator" target="_blank" rel="noreferrer">Calculator</a>
-                                <div className="dropdown-content">
-                                    <p>Fun Calculator made with React, HTML, CSS and javascript.</p>
-                                </div>
-                            </div>
-                            <div className="dropdown">
                                 <a className="dropbtn" href="https://github.com/FullStackJustin/Netflix-Browse-Titles-frontend-clone" target="_blank" rel="noreferrer">Netflix Clone</a>
                                 <div className="dropdown-content">
                                     <p>Frontend replica of Netflix's browse titles page. This project fetches and renders REST api data and was built with React, html, css, and javascript </p>
@@ -62,19 +56,17 @@ export const Screen2 = () => {
                                 </div>
                             </div>
                             <div className="dropdown">
-                                <a className="dropbtn" href="https://github.com/FullStackJustin/todo-w-fetch-and-react" target="_blank" rel="noreferrer">Todo list w/ fetch</a>
-                                <div className="dropdown-content">
-                                    <p >Todolist with a Cool frontend design that uses get and delete method to update todo list</p>
-                                </div>
-                            </div>
-                            <div className="dropdown">
                                 <a className="dropbtn" href="https://github.com/FullStackJustin/Auth-system-w-firebase-template" target="_blank" rel="noreferrer">Auth System Template</a>
                                 <div className="dropdown-content">
                                     <p>Complete authentication system with customizable homepage, login, signup, and account page built with firebase as backend, tailwind for styling, react, and javascript</p>
                                 </div>
                             </div>
-
-
+                            <div className="dropdown">
+                                <a className="dropbtn" href="https://miyareads.netlify.app" target="_blank" rel="noreferrer">Personal Blog</a>
+                                <div className="dropdown-content">
+                                    <p>Personal blog website built for a client using Firebase for auth and database storage, react, javascript, tailwind, node and express</p>
+                                </div>
+                            </div>
                         </main>
                     </form>
                 </div>
@@ -89,13 +81,13 @@ export const Screen2 = () => {
                         <main>
                             <div className="aboutMainDiv">
                                 <div className="columnText">
-                                    <h3>Hello! My name is Justin Taylor and I am a fullstack web developer mainly specializing in react and javascript.</h3>
-                                    <p><strong>Tech I have worked with </strong>: Javscript, HTML, CSS, React, Node, Express, Tailwind, Firebase, Bootstrap, Jest, Python, sqlAlchemy, Redux, Flask, Postman, fullpage.js, next.js, and email.js   </p>
+                                    <h3>Hello! My name is Justin Taylor and I am a fullstack web developer specializing in react and javascript.</h3>
+                                    <p><strong>Tech I have worked with </strong>: Javscript, HTML, CSS, React, Node, Express, Tailwind, Firebase, Bootstrap, Jest, Python, sqlAlchemy, Redux, Flask, Postman, fullpage.js, and email.js   </p>
                                     <h4>Fun Facts about me : </h4>
                                     <p>I especially like working with front end.</p>
                                     <p>I have been audio engineering since 2013. </p>
                                     <p>My favorite shows ever are Black Clover and The Last Kingdom.</p>
-                                    <p>My hobbies include making music, exploring, ps4 with friends, and playing with my dog.</p>
+                                    <p>My hobbies include making music, travel & exploring, watching anime, playing ps4 with friends, and playing with my dog.</p>
 
 
                                 </div>
@@ -112,9 +104,24 @@ export const Screen2 = () => {
                         <main>
                             <div className="workMainDiv">
                                 <div className="columnText">
-                                    <p>I am actively searching for my first professional role in either a frontend or fullstack position.</p>
-                                    <br/>
+                                    <p>I am a junior developer actively searching for a web development position.</p>
                                     <p>My previous work experience includes customer service, d2d sales, warehouse associate roles, and delivery operations. Feel free to view my LinkedIn for full scope of my work history.</p>
+                                    <h5><strong>Education</strong></h5>
+                                    <table>
+                                        <tbody>
+                                            <tr>
+                                                <td><strong>Title</strong></td>
+                                                &ensp;
+                                                <td><strong>Description</strong></td>
+                                            </tr>
+                                            <br/>
+                                            <tr>
+                                                <td>4Geeks Academy-Remote</td>
+                                                &ensp;
+                                                <td>Online bootcamp where I studied fullstack web development for 6 months learning React, javascript, pyhton, sqlAlchemy, flask, redux, html, css, and bootstrap. (Completed with certificate)</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
                                 </div>
                             </div>
                         </main>
